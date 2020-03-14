849d88075985142bd1fbd6a8cfa7a354f11d0db5
Chingiz Mamedov <38461842+chingiz92mi@users.noreply.github.com>
2020-03-14 19:37:30 +0400

    set delete btn

--- js/baki.js
+++ js/baki.js
@@ -6,6 +6,26 @@ $(function() {
 	var svg = document.getElementById("baki-full"),
 		NS = svg.getAttribute("xmlns");
 
+	const drowCircles = () => {
+		var target = document.getElementById("baki-full");
+
+		var element = document.getElementsByTagName("circle"),
+			index;
+
+		for (index = element.length - 1; index >= 0; index--) {
+			element[index].parentNode.removeChild(element[index]);
+		}
+
+		for (var i = 0; i < allFilials.length; i++) {
+			circle = document.createElementNS(NS, "circle");
+
+			circle.setAttributeNS(null, "cx", allFilials[i].filialX);
+			circle.setAttributeNS(null, "cy", allFilials[i].filialY);
+			circle.setAttributeNS(null, "r", 2);
+			target.appendChild(circle);
+		}
+	};
+
 	// debugger
 	// add a circle to the SVG
 	svg.addEventListener(
@@ -63,46 +83,51 @@ $(function() {
 		redrow();
 	});
 
+	const removeElementFromAllFilters = index => {
+		allFilials.splice(index, 1);
+	};
+
+	const setDeleteButton = () => {
+		const buttons = document.getElementsByClassName("delete-item");
+
+		Object.keys(buttons).forEach(item => {
+			buttons[item].onclick = function(e) {
+				this.parentNode.parentNode.parentNode.removeChild(
+					this.parentNode.parentNode,
+				);
+				removeElementFromAllFilters(e.target.getAttribute("data-id"));
+				console.log("all filiasl", allFilials);
+				drowCircles();
+			};
+		});
+	};
+
 	function redrow() {
-		var target = document.getElementById("baki-full");
-
-		var element = document.getElementsByTagName("circle"),
-			index;
-
-		for (index = element.length - 1; index >= 0; index--) {
-			element[index].parentNode.removeChild(element[index]);
-		}
-
-		for (var i = 0; i < allFilials.length; i++) {
-			circle = document.createElementNS(NS, "circle");
-
-			circle.setAttributeNS(null, "cx", allFilials[i].filialX);
-			circle.setAttributeNS(null, "cy", allFilials[i].filialY);
-			circle.setAttributeNS(null, "r", 2);
-			target.appendChild(circle);
-		}
+		drowCircles();
 		var listing = "";
 		for (var i = 0; i < allFilials.length; i++) {
 			let toast = `<div class="toast opacity-1" role="alert" aria-live="assertive" aria-atomic="true">
-                      <div class="toast-header">
-                      <span class="badge badge-secondary mr-2">${i + 1}</span>
-                        <strong class="mr-auto">${
-													allFilials[i].filialName
-												}</strong>
-                        
-                        <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-                          <span aria-hidden="true">&times;</span>
-                        </button>
-                      </div>
-                      <div class="toast-body">
-                        <span>X : ${allFilials[i].filialX}</span>
-                        <span>Y: ${allFilials[i].filialY}</span>
-                      </div>
-                    </div>`;
+                            <div class="toast-header">
+                            <span class="badge badge-secondary mr-2">${i +
+															1}</span>
+                                <strong class="mr-auto">${
+																	allFilials[i].filialName
+																}</strong>
+                                
+                                <button data-id="${i}" class="delete-item" type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="toast-body">
+                                <span>X : ${allFilials[i].filialX}</span>
+                                <span>Y: ${allFilials[i].filialY}</span>
+                            </div>
+                            </div>`;
 			listing += toast;
 		}
 		document.getElementById("listFull").innerHTML = listing;
 		console.log(allFilials);
+		setDeleteButton();
 	}
 
 	redrow();
